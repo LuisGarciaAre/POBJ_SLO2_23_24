a30a8dbf2da79318e35ec1922a2c162083ebd8b2
Luis <107997427+LuisGarciaAre@users.noreply.github.com>
2023-12-19 15:49:43 +0100

    partie A OK

--- Exercices Luis/Exercice 2/Ex2_fonctions/Ex2.cpp
+++ Exercices Luis/Exercice 2/Ex2_fonctions/Ex2.cpp
@@ -20,34 +20,65 @@ int main (void)
 {
 	char UserAnswer;
 	int ValA, ValB;
+	float heures, min, sec, reste;
 	// Variables pour test A, B et C
 	
 
 	// a adapter
-	cout << "Exercice 2 : Huber Christian" << endl;
+	cout << "Exercice 2 : Luis Garcia" << endl;
 
 
 	do {
 		cout << "Test A, B ou C, Q pour Quitter" << endl;
 		cin >> UserAnswer;
+		UserAnswer = std::toupper(UserAnswer);
 		
 		switch (UserAnswer) {
 			case 'A':
-			case 'a':
-				cout << "TestA: entrez un nombre de secondes" << endl;
+				cout << "TestA: entrez un nombre de secondes: ";
 				cin >> ValA;
-				
+
+				if ((std::cin.fail() == true))
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << "La valeur introduite n'est une valeur entiere" << std::endl;
+					std:cout << "\n" << std::endl;
+				}
+				else
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					if (ValA >= 3600)
+					{
+						heures = ValA / 3600.0f;
+						reste = heures - static_cast<int>(heures);
+						min = reste * 60.0f;
+						reste = min - static_cast<int>(min);
+						sec = reste * 60.0f;
+					}
+					else
+					{
+						min = ValA / 60.0f;
+						reste = min - static_cast<int>(min);
+						sec = reste * 60.0f;
+						heures = 0;
+					}
+
+					std::cout << std::fixed << std::setprecision(0) << "Val en secondes = " << ValA << "-> Heures: " << heures << ", minutes: " << min << ", secondes: " << sec << std::endl;
+					std::cout << "\n" << std::endl;
+				}
+
+
 			break;
 
 			case 'B':
-			case 'b':
 				cout << "TestB: entrez un nombre " <<  endl;
 				cin >> ValB; 
 				
 			break;
 
 			case 'C':
-			case 'c':
 				cout << "TestC: calculs surface " <<  endl;
 
 				// a)	Avec 2 paramètres int 25 et 45
@@ -61,7 +92,7 @@ int main (void)
 
 		} // end switch
 	
-	} while (!(UserAnswer == 'Q' || UserAnswer == 'q'));
+	} while (!(UserAnswer == 'Q'));
 
 	return (0);
 }
